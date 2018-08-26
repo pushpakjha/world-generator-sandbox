93cb60dfa200a7f0209d27c0d845c2f72dd71828
pushpakjha <ppjha0@gmail.com>
2018-08-26 11:59:48 -0600

    Create main world execution structure

---
+++ simulate_world.py
@@ -0,0 +1,27 @@
+# Main simulation loop file
+import collections
+
+
+class World(object):
+    """Main world object.
+    """
+    def __init__(self):
+        self.time = 0
+        self.world_map = collections.defaultdict(dict)
+
+
+class SimulateWorld(object):
+    """Execute the simulation on a world object.
+
+    :param World world: The world object
+    :param int end_time: Number of seconds to simulate world
+    """
+    def __init__(self, world, end_time):
+        self.world = world
+        self.end_time = end_time
+
+    def execute(self):
+        """Main execute function, runs in a loop until time has elapsed."""
+        while self.end_time:
+            self.execute_second()
+            self.end_time -= 1
