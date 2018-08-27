7b3de7827c9a127cc8bd8d3aac5b62ae913352ca
pushpakjha <ppjha0@gmail.com>
2018-08-26 22:09:29 -0600

    Change logic for simulation loop, first attempt at basic bacteria logic

--- simulate_bacteria.py
+++ simulate_bacteria.py
@@ -6,30 +6,37 @@ class Bacteria(object):
     """Base bacteria object.
 
     :param int max_lifetime: How long the bacteria should live
-    :param list[Bacteria] global_bacteria: List of all the bacteria
     :param int child_counter: The id of the parent to this child, if this is a child else None
     """
-    def __init__(self, max_lifetime, global_bacteria, child_counter=None, reproduction_rate=5):
+    def __init__(self, max_lifetime, child_counter=None, reproduction_rate=5):
         self.current_lifetime = 0
         self.max_lifetime = max_lifetime
-        self.global_bacteria = global_bacteria
         self.child_counter = child_counter
         self.reproduction_rate = reproduction_rate
 
-    def execute_second(self):
-        """Add to the lifetime and perform basic life checks."""
+    def execute_second(self, world):
+        """Add to the lifetime and perform basic life checks.
+
+         :param sandbox.simulation_world.World world: The world object
+        """
         self.current_lifetime += 1
         if self.current_lifetime >= self.max_lifetime:
-            self.die()
+            self.die(world)
         if self.current_lifetime % self.reproduction_rate == 0:
-            self.reproduce()
+            self.reproduce(world)
 
-    def die(self):
-        """Kill the bacteria."""
-        self.global_bacteria.remove(self)
+    def die(self, world):
+        """Kill the bacteria.
 
-    def reproduce(self):
-        """Make a new child bacteria."""
+        :param sandbox.simulation_world.World world: The world object
+        """
+        world.global_bacteria.remove(self)
+
+    def reproduce(self, world):
+        """Make a new child bacteria.
+
+        :param sandbox.simulation_world.World world: The world object
+        """
         self.child_counter += 1
         child = self(self.max_lifetime,  self.global_bacteria, self.child_counter, self.reproduction_rate)
-        self.global_bacteria.append(child)
+        world.global_bacteria.append(child)
--- simulate_world.py
+++ simulate_world.py
@@ -3,11 +3,11 @@ import collections
 
 
 class World(object):
-    """Main world object.
-    """
+    """Main world object."""
     def __init__(self):
         self.time = 0
         self.world_map = collections.defaultdict(dict)
+        self.global_bacteria = []
 
 
 class SimulateWorld(object):
@@ -19,13 +19,16 @@ class SimulateWorld(object):
     def __init__(self, world, end_time):
         self.world = world
         self.end_time = end_time
+        self.global_bacteria = world.global_bacteria
 
     def execute(self):
         """Main execute function, runs in a loop until time has elapsed."""
         while self.end_time:
             self.execute_second()
             self.end_time -= 1
+            self.world.time += 1
 
     def execute_second(self):
         """Run one second of the simulation."""
-        pass
+        for bacteria in self.global_bacteria:
+            bacteria.execute_second(self.world)
