d6dea77a0696efb576acad170b804fb2068fbd50
pushpakjha <ppjha0@gmail.com>
2018-09-01 13:24:55 -0600

    Clean up execution script

--- execute_simulation.py
+++ execute_simulation.py
@@ -3,7 +3,9 @@ import sandbox
 
 
 if __name__ == "__main__":
-    world = sandbox.World(20, 20)
-    initial_bacteria = [sandbox.OxygenBacteria(2, 2), sandbox.NitrogenBacteria(4, 4), sandbox.PhosphorusBacteria(6, 6)]
-    simulate_world = sandbox.SimulateWorld(world, 100, initial_bacteria=initial_bacteria)
+    world = sandbox.World(max_x_size=20, max_y_size=20)
+    initial_bacteria = [sandbox.OxygenBacteria(x_position=2, y_position=2),
+                        sandbox.NitrogenBacteria(x_position=4, y_position=4),
+                        sandbox.PhosphorusBacteria(x_position=6, y_position=6)]
+    simulate_world = sandbox.SimulateWorld(world=world, end_time=100, initial_bacteria=initial_bacteria)
     simulate_world.execute()
